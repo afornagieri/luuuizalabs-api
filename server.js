e3b2e8cd2f51929ba9418e164b65f5d92d73879a
Artur Fornagieri <fornagieri.artur@gmail.com>
2025-04-26 11:16:55 -0300

    Update import

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import { listen } from './src/app';
+import app from './src/app.js';
 const PORT = process.env.PORT || 3000;
 
-listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
