549de8ad8b00fcde1fedf42d275f0bc71b30141c
Artur Fornagieri <fornagieri.artur@gmail.com>
2025-04-26 18:38:10 -0300

    Add generic error handler middleware

---
+++ src/middlewares/errorHandlerMiddleware.js
@@ -0,0 +1,20 @@
+const {
+    BadRequestError,
+    UnauthorizedError,
+    NotFoundError,
+    InternalServerError
+} = require('../utils/customErrorHandler.js');
+
+function errorHandler(err, _, res, _) {
+    console.error(err);
+
+    if (err instanceof BadRequestError ||
+        err instanceof UnauthorizedError ||
+        err instanceof NotFoundError ||
+        err instanceof InternalServerError) {
+        return res.status(err.statusCode).json({ error: err.message });
+    }
+    res.status(500).json({ error: 'Internal Server Error' });
+}
+
+module.exports = errorHandler;
