a4f0c1f759b074f5d360f17e69c4fcbd42500ed0
Artur Fornagieri <fornagieri.artur@gmail.com>
2025-04-27 09:37:21 -0300

    Add missing customer routes, controllers, services and repositories

--- src/services/customerService.js
+++ src/services/customerService.js
@@ -1,9 +1,39 @@
-const customerRepository =  require('../repositories/index.js');
+const customerRepository = require('../repositories/customerRepository');
+const { BadRequestError } = require('../utils/customErrorHandler');
 
-function getCustomersAsync() { 
-    return customerRepository.getAll();
+async function getCustomersAsync() {
+  return await customerRepository.getAll();
+}
+
+async function createCustomerAsync(data) {
+  const { name, email, favorite_products } = data;
+
+  if (!name || !email) {
+    throw new BadRequestError('Name and email are required');
+  }
+
+  if (favorite_products && (!Array.isArray(favorite_products) || !favorite_products.every(Number.isInteger))) {
+    throw new BadRequestError('favorite_products must be an array of integers');
+  }
+
+  return await customerRepository.create({ name, email, favorite_products });
+}
+
+async function updateCustomerAsync(id, data) {
+  if (data.favorite_products && (!Array.isArray(data.favorite_products) || !data.favorite_products.every(Number.isInteger))) {
+    throw new BadRequestError('favorite_products must be an array of integers');
+  }
+
+  return await customerRepository.update(id, data);
+}
+
+async function deleteCustomerAsync(id) {
+  return await customerRepository.remove(id);
 }
 
 module.exports = {
-    getCustomersAsync
-}
+  getCustomersAsync,
+  createCustomerAsync,
+  updateCustomerAsync,
+  deleteCustomerAsync
+};
